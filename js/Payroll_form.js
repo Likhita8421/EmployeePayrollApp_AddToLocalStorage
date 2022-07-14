5b460f823470f919d87913f7b69ce8e18495048e
Likhita <leenupandagre24@gmail.com>
2022-07-14 08:47:00 +0530

    UC1- UC5

---
+++ js/Payroll_form.js
@@ -0,0 +1,182 @@
+//Salary Range
+
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+output.textContent = salary.value; });
+
+
+
+//UC-2 New Payroll Form - Add EventListeners
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent = " ";
+            return;
+        }
+        try {
+            ( new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        }
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+});
+
+
+//UC3 - Create Employee payroll Object On Save.
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } 
+    catch (e) {
+        return;
+    }
+}
+
+function createAndUpdateStorage(employeePayrollData) {                                               // 
+
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));              //
+                           
+    if(employeePayrollList !=undefined) {                                                          
+        employeePayrollList.push(employeePayrollData);                                             ///UC-4  Save Employee payroll Object to local Storage
+    }
+    else {                                                                                           
+        employeePayrollList = [employeePayrollData]
+    }                                                                                              //
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify("employeePayrollList"))             //
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try{
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch(e) {
+        setTextValue('.text-error',e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]').pop();
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+ getInputValueById('#year');               
+    employeePayrollData.startDate = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getInputValueById = (id) =>{
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItem = [];
+    allItems.forEach(item => {
+        if(item.checked){
+            selItem.push(item.value);
+        }
+    });
+    return selItem;
+}
+
+/*
+* 1: querySelector is the newer feature.
+* 2: The querySelector method can be used when selecting by element name,
+*    nesting, or class name.
+* 3: querySelector lets you find elements with rules that can't be
+*    expressed with getElemetById
+*/
+
+const getElemetById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+/*
+* 1: getElementId is better supported than querySelector in older versions
+* of the browsers.
+* 2: The thing with getElementId is that it only allows to select an
+* element by its id.
+*/
+
+const getSelectedValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+
+//UC5- Reset the form on clicking Reset.
+    //instead of reset method we can call form button reset
+    
+const resetForm = () => {
+    setValue('name',"");
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
+}    
+
+const  unsetSelectedValues =(propertyValue) => {
+    const allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
+
+
+footer
+
+
+
+
+
+
+
+
+                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
